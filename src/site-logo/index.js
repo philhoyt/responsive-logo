c275923f7b7674933216ebb1d96e0220452dea48
Phil Hoyt <philhoyt@users.noreply.github.com>
2026-04-21 11:44:22 -0400

    Use core site logo block icon

--- src/site-logo/index.js
+++ src/site-logo/index.js
@@ -1,8 +1,10 @@
 import { registerBlockType } from '@wordpress/blocks';
+import { siteLogo } from '@wordpress/icons';
 import metadata from './block.json';
 import Edit from './edit';
 
 registerBlockType( metadata.name, {
+	icon: siteLogo,
 	edit: Edit,
 	save: () => null,
 } );
